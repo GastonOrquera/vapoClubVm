9f1965a849892f0aca30264d0f5084a8bce70edb
GastonOrquera <gastonorquera29@gmail.com>
2024-03-05 03:52:10 -0300

    vapo

---
+++ js/main.js
@@ -0,0 +1,47 @@
+
+let hamburger = document.querySelector('.hamburger');
+
+let nav = document.querySelector('nav');
+
+let navLinks = document.querySelectorAll('nav a');
+
+hamburger.addEventListener('click', function () {
+
+    nav.classList.toggle('active');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            nav.classList.remove('active');
+        });
+    });
+
+});
+
+
+
+
+
+
+ScrollReveal().reveal('.scroll-revealImg', {
+    origin: 'left',
+    distance: '4px',
+    duration: 1000,
+    transition: 'cubic-bezier(0.5,0,0,1)',
+    interval: 200
+});
+
+ScrollReveal().reveal('.scroll-revealTitles', {
+    origin: 'bottom',
+    distance: '14px',
+    duration: 1000,
+    transition: 'cubic-bezier(0.5,0,0,1)',
+    interval: 200
+});
+
+ScrollReveal().reveal('.scroll-revealAncor', {
+    origin: 'left',
+    distance: '40px',
+    duration: 600,
+    transition: 'cubic-bezier(0.5,0,0,1)',
+    interval: 400
+});
